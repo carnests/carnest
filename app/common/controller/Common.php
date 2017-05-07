ecc798fa2d778fdefb4332efaa6ebc0614f741c4
张栋 <420026004@qq.com>
2017-05-07 23:26:42 +0800

    2017-5-7 添加两个加密方法

--- app/common/controller/Common.php
+++ app/common/controller/Common.php
@@ -8,12 +8,16 @@
  */
 namespace app\common\controller;
 use app\auth\controller\Mp;
+use app\common\tool\ed\Crypt3Des;
+
 class Common extends Mp
 {
+    protected $_3d_key;     //加密密码
     public function __construct()
     {
         parent::__construct();
         $this->indexDate();
+        $this->_3d_key = 'd554d3f16c6f4d4ac0f79c4ae43492b2';
     }
 
     protected function indexDate()
@@ -23,5 +27,28 @@ class Common extends Mp
             'public_base' => '../app/common/view/base.html',
             'js' => $this->wechat->js,
         ]);
+    }
+
+
+    /**
+     * 加密
+     * @param $str
+     * @return string
+     */
+    public function _encode($str)
+    {
+        $crypt = new Crypt3Des($this->_3d_key, 'ECB', 'off');
+        return $crypt->encrypt($str, 'hex');
+    }
+
+    /**
+     * 解密
+     * @param $str
+     * @return string
+     */
+    public function _decode($str)
+    {
+        $crypt = new Crypt3Des($this->_3d_key, 'ECB', 'off');
+        return $crypt->decrypt($str, 'hex');
     }
 }
